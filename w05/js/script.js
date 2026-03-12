8ee0edce5650228b9679748817c11d5d32961f6e
Elijah <elijahdasilva06@gmail.com>
2026-03-12 03:35:52 -0400

    e

--- w05/js/script.js
+++ w05/js/script.js
@@ -1,16 +1,49 @@
-$("#preamble-btn-1").click(function(){
-    $("#preamble-btn-2").css({
-        "background":"blue",
-        "transform":"rotate(45deg)"
+
+
+// $("#preamble-btn-1").click(function(){
+//     $("#preamble-btn-2").css({
+//         "background":"blue",
+//         "transform":"rotate(45deg)"
         
-    })
-})
+//     })
+// })
 
-$("#preamble-btn-2").click(function(){
-    $("#preamble-btn-1").toggleclass("rotate")
+// $("#preamble-btn-2").click(function(){
+//     $("#preamble-btn-1").toggleclass("rotate")
         
+//     })
+
+// $("#h1.header-left").click(function(){
+
+//     ("#h1.header-left").toggleClass("active")
+
+    $(document).scroll(function(){
+        let distance = $(document).scrollTop()
+        let height = $(document).height()-$(window).height();
+        let ratio = distance/height();
+
+        $((">header-right").html(ratio))
+
+        $(".header-right").html("you have travelled " + distance + " pixels so far")
+
+        $("h1").css({
+            "transform":"rotate(" + ratio*360 + "deg)"
+
+
+
+
+
+        })
     })
 
-$("#h1").click(function(){
+// })
 
-    ("#menu").toggleClass("active")
+$(".album-thumbs").flickity({
+
+    wrapAround: true,
+    // freeScroll: true,
+    // contain: true,
+    // disable previous & next buttons and dots
+    prevNextButtons: false,
+    pageDots: false
+});
